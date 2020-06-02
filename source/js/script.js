2c703d8e80268808679e89bb9adac1a79d82d3b8
Viktor Kan <html.viktor@gmail.com>
2020-06-02 22:25:58 +0300

    make home page styles

--- source/js/script.js
+++ source/js/script.js
@@ -1,40 +1,49 @@
 import fullpage from 'fullpage.js';
 import Swiper from 'swiper';
 
-const navLinks = document.querySelectorAll('.nav__link');
+const navElement = document.querySelector('.nav');
+const benefitsElements = document.querySelectorAll('.benefits__nav-link');
+
+benefitsElements.forEach(element => {
+    element.addEventListener('click', evt => {
+        evt.preventDefault();
+        benefitsElements.forEach(el => {
+            el.classList.remove('benefits__nav-link--active')
+        })
+        element.classList.add('benefits__nav-link--active')
+    })
+})
 
 const colorizeSlides = (index) => {
     if(index !== 0) {
-        navLinks.forEach(el => {
-            el.style.color = 'black';
-        })
+        navElement.classList.remove('nav-white');
+        navElement.classList.add('nav-black');
     } else {
-        navLinks.forEach(el => {
-            el.style.color = 'white';
-        })
+        navElement.classList.remove('nav-black')
+        navElement.classList.add('nav-white');
     }
-     switch (index) {
-        case 0:
-            $('.fp-tooltip').css('color', '#333');
-            $('#fp-nav span').css('background', '#333');
-            break;
-        case 1:
-            $('.fp-tooltip').css('color', 'white');
-            $('#fp-nav span').css('background', 'white');
-            break;
-        case 2:
-            $('.fp-tooltip').css('color', '#333');
-            $('#fp-nav span').css('background', '#333');
-            break;
-        case 3:
-            $('.fp-tooltip').css('color', 'white');
-            $('#fp-nav span').css('background', 'white');
-            break;
-        default:
-            $('.fp-tooltip').css('color', 'white');
-            $('#fp-nav span').css('background', 'white');
-            break;
-    }
+    //  switch (index) {
+    //     case 0:
+    //         $('.fp-tooltip').css('color', '#333');
+    //         $('#fp-nav span').css('background', '#333');
+    //         break;
+    //     case 1:
+    //         $('.fp-tooltip').css('color', 'white');
+    //         $('#fp-nav span').css('background', 'white');
+    //         break;
+    //     case 2:
+    //         $('.fp-tooltip').css('color', '#333');
+    //         $('#fp-nav span').css('background', '#333');
+    //         break;
+    //     case 3:
+    //         $('.fp-tooltip').css('color', 'white');
+    //         $('#fp-nav span').css('background', 'white');
+    //         break;
+    //     default:
+    //         $('.fp-tooltip').css('color', 'white');
+    //         $('#fp-nav span').css('background', 'white');
+    //         break;
+    // }
 }
 
 
@@ -42,7 +51,7 @@ const colorizeSlides = (index) => {
     scrollHorizontally: true,
     navigation: $(document).width() <= 760 ? false : true,
     navigationPosition: 'left',
-    navigationTooltips: ['Home', 'Works', 'Our Services', 'Our Story'],
+    navigationTooltips: ['Home', 'History', 'Recipes', 'Testimonials', 'Special Offers', 'Contact Us'],
     verticalCentered: false,
     afterLoad(origin, destination, direction) {
         colorizeSlides(destination.index)
