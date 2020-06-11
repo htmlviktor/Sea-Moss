bfb52ef784372556b975d232e36b01ca5bbb2d62
htmlviktor <47639976+htmlviktor@users.noreply.github.com>
2020-06-11 11:25:29 +0300

    make nav menu mobile

--- source/js/script.js
+++ source/js/script.js
@@ -3,3 +3,11 @@ import HomePage from './pages/home-page';
 const homePage = new HomePage('.home-page');
 
 homePage.init()
+
+
+const toggleMenu = document.querySelector('.nav__toggle');
+const navElement = document.querySelector('.nav');
+
+toggleMenu.addEventListener('click', () => {
+    navElement.classList.toggle('nav--open')
+})
